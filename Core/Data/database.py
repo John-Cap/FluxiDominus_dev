cfb42bf501f690827a06bdc1f2d6ae2530473f3c
John-Cap <bonnet.wessel@gmail.com>
2024-09-10 10:59:32 +0200

    Seventh

--- Core/Data/database.py
+++ Core/Data/database.py
@@ -386,25 +386,15 @@ class DatabaseOperations:
         if (isinstance(_userProj,str)):
             _userProj=eval(_userProj)
         for _x in _userProj: #For each project id, get its name, associated tests, and each test's testruns
-            _projName=self.getProjCode(_x)
-            _projTests=[]
-            _desc=""
+            _ret['ExpWidgetInfo'][str(_x)]={}
             for _y in self.mySqlDb.fetchRecordsByColumnValues(tableName='testlist',column1Name='projId',value1=_x,column2Name='userId',value2=_userId):
-                _projTests.append(_y[0])
-                _desc=_y[3]
-                _labNotebookBaseRef=_y[4]
-                _datetimeCreate=_y[5]
-            for _y in _projTests: #Get runs, complete rows converted to lists
-                _ret['ExpWidgetInfo'][str(_x)]={
-                    'projCode':_projName,
-                    str(_y):{
-                        "testruns":self.getTestrunsByTestId(_y),
-                        "description":_desc,
-                        "labNotebookBaseRef":_labNotebookBaseRef,
-                        "datetimeCreate":_datetimeCreate.isoformat()
-                    }
+                _ret['ExpWidgetInfo'][str(_x)][str(_y[0])]={
+                    'projCode':self.getProjCode(_x),
+                    "testruns":self.getTestrunsByTestId(_y[0]),
+                    "description":_y[3],
+                    "labNotebookBaseRef":_y[4],
+                    "datetimeCreate":_y[5].isoformat()
                 }
-        print(json.dumps(_ret))
         return json.dumps(_ret)
 
     def getUserProjsDet(self,orgId=None,email=None):
