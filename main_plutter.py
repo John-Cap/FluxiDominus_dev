099af5ff783a050f174ee3fb8606c0a535ace0e9
John_Cap <bonnet.wessel@gmail.com>
2024-10-23 20:55:16 +0200

    Additional 3

--- main_plutter.py
+++ main_plutter.py
@@ -1,29 +1,224 @@
-from datetime import datetime
-from Core.Data.database import DatabaseOperations, DatabaseStreamer, MySQLDatabase, TimeSeriesDatabaseMongo
-from Core.UI.plutter import MqttService
-
-
-if __name__ == '__main__':
-    #Mqtt
-    thisThing=MqttService()
-    thisThing.start()
-    thisThing.orgId="50403"
-    #Instantiate
-    mySqlDb=MySQLDatabase(host='146.64.91.174')
-    mongoDb=TimeSeriesDatabaseMongo(host='146.64.91.174')
-
-    dbStreaming=DatabaseStreamer(mySqlDb=mySqlDb,mongoDb=mongoDb,mqttService=thisThing)
-    dbStreaming.connect()
-    dbStreaming.mongoDb.currZeroTime=datetime.now()
-    dbStreaming.handleStreamRequest(
-        {
-            "id":"123",
-            "labNotebookBaseRef":"50403_jdtoit_DSIP012A",
-            "runNr":0,
-            "timeWindow":30,
-            "nestedField":None,
-            "nestedValue":None,
-            "deviceName":"flowsynmaxi2",
-            "setting":"pafr"
-        }
-    )
+{
+  "vapourtecR4P1700": {
+    "pafr": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "PumpAFlowRate",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "pbfr": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "PumpBFlowRate",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "svasr": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "valveASR",
+          "value": False
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "svbsr": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "valveBSR",
+          "value": False
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "svcw": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "subDevice": "valveCW",
+          "command": "SET",
+          "value": False
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "svail": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "valveAIL",
+          "value": False
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "svbil": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "valveBIL",
+          "value": False
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "r1temp": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "Reactor1Temp",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "r2temp": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "Reactor2Temp",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "r3temp": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "Reactor3Temp",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    },
+    "r4temp": {
+      "address": ["settings","value"],
+      "cmnd": {
+        "deviceName": "vapourtecR4P1700",
+        "inUse": True,
+        "connDetails": {
+          "ipCom": {
+            "addr": "192.168.1.53",
+            "port": 43344
+          }
+        },
+        "settings": {
+          "command": "SET",
+          "subDevice": "Reactor4Temp",
+          "value": 0
+        },
+        "topic": "subflow/vapourtecR4P1700/cmnd",
+        "client": "client"
+      }
+    }
+  }
+}
