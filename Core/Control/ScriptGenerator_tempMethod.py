42da8332d5245c69bc9a9f63f6bbe0d1e48d397c
John_Cap <bonnet.wessel@gmail.com>
2024-10-23 13:52:52 +0200

    Additional 2

--- Core/Control/ScriptGenerator_tempMethod.py
+++ Core/Control/ScriptGenerator_tempMethod.py
@@ -361,7 +361,203 @@ class FlowChemAutomation:
                     "client":"client"
                 }
             ''',
-            
+            #Vapourtec R4 P1700
+            "vapourtecR4P1700_pafr":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "PumpAFlowRate",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_pbfr":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "PumpBFlowRate",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            #Switch valve A solv/reag
+            "vapourtecR4P1700_svasr":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse": True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings":{"command":"SET", "subDevice":"valveASR", "value":>bool<},
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            #Switch valve B solv/reag
+            "vapourtecR4P1700_svbsr":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse": True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings":{"command":"SET","subDevice":"valveBSR","value":>bool<},
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_svcw":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse": True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "subDevice": "valveCW",
+                        "command": "SET",
+                        "value": >bool<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            #Switch valve A inject/load
+            "vapourtecR4P1700_svail":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse": True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings":{"command":"SET", "subDevice":"valveAIL", "value": >bool<},
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_svbil":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse": True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings":{"command":"SET", "subDevice":"valveBIL", "value": >bool<},
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_r1temp":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "Reactor1Temp",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_r2temp":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "Reactor2Temp",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_r3temp":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "Reactor3Temp",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
+            "vapourtecR4P1700_r4temp":'''
+                {
+                    "deviceName": "vapourtecR4P1700",
+                    "inUse":True,
+                    "connDetails": {
+                        "ipCom": {
+                            "addr": "192.168.1.53",
+                            "port": 43344
+                        }
+                    },
+                    "settings": {
+                        "command": "SET",
+                        "subDevice": "Reactor4Temp",
+                        "value": >float<
+                    },
+                    "topic":"subflow/vapourtecR4P1700/cmnd",
+                    "client":"client"
+                }
+            ''',
             #Custom commands
             "Delay":'''
                 {"Delay": {"initTimestamp": None, "sleepTime": >float<}}
@@ -416,6 +612,19 @@ class FlowChemAutomation:
                 "svcw":self.commandTemplates["flowsynmaxi2_svcw"],
                 "svia":self.commandTemplates["flowsynmaxi2_svia"],
                 "svib":self.commandTemplates["flowsynmaxi2_svib"]
+            },
+            "vapourtecR4P1700":{
+                "pafr":self.commandTemplates["vapourtecR4P1700_pafr"],
+                "pbfr":self.commandTemplates["vapourtecR4P1700_pbfr"],
+                "svail":self.commandTemplates["vapourtecR4P1700_svail"],
+                "svbil":self.commandTemplates["vapourtecR4P1700_svbil"],
+                "svcw":self.commandTemplates["vapourtecR4P1700_svcw"],
+                "svasr":self.commandTemplates["vapourtecR4P1700_svasr"],
+                "svbsr":self.commandTemplates["vapourtecR4P1700_svbsr"],
+                "temp1":self.commandTemplates["vapourtecR4P1700_r1temp"],
+                "temp2":self.commandTemplates["vapourtecR4P1700_r2temp"],
+                "temp3":self.commandTemplates["vapourtecR4P1700_r3temp"],
+                "temp4":self.commandTemplates["vapourtecR4P1700_r4temp"]
             }
         }
 
